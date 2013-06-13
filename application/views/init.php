254af65315fc8570f1eca73c2f14477285dccf26
dfkuro <izmirreffi@gmail.com>
2013-06-13 00:49:09 -0500

    First Commit to add

---
+++ application/views/init.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+</head>
+<body>
+
+<div id="container">
+	<h1>Dfkuro init</h1>
+
+	<div id="body">
+		
+	</div>
+
+	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+</div>
+
+</body>
+</html>
